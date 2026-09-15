1ef900dd63308fcc8215061bb1d7a9108f6c3ba5
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-09-15 16:38:50 +0700

    Delete Frontend directory

--- Frontend/build/client/assets/with-props-ap4d8DRX.js
+++
@@ -1,1 +0,0 @@
-import{x as p,y as t,z as e,w as o,r as s,A as u}from"./chunk-GNGMS2XR-BqdPGk2M.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
